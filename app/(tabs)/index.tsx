5a615fc26df8dc2f2ec480bc1c2218efa97bcc1c
KenetCh <57511132+KenetAndres-gitHub@users.noreply.github.com>
2025-04-02 16:17:51 -0500

    Updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,71 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, Image, TextInput, Button, SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons'; // Importa la biblioteca de íconos
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import profileImageLocal from '../../assets/images/profile1.jpeg';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [savedPhoneNumber, setSavedPhoneNumber] = useState('999999999999');
+
   const user = {
     name: 'Kenet Chungandro',
     email: 'kenbok2018@gmail.com',
     profileImage: profileImageLocal,
   };
 
-  const ProfileCard = ({ name, email }) => (
-    <View style={styles.profileCard}>
+  const ProfileCard = ({ name, email, phone }) => (
+    <LinearGradient
+      colors={['#045e96', '#008160']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.profileCard}
+    >
       <View style={styles.row}>
-        <Icon name="person" size={24} color="gray" style={styles.icon} />
+        <Icon name="person" size={24} color="white" style={styles.icon} />
         <Text style={styles.name}>{name}</Text>
       </View>
       <View style={styles.row}>
-        <Icon name="email" size={24} color="gray" style={styles.icon} />
+        <Icon name="email" size={24} color="white" style={styles.icon} />
         <Text style={styles.email}>{email}</Text>
       </View>
-    </View>
+      <View style={styles.row}>
+        <Icon name="phone" size={24} color="white" style={styles.icon} />
+        <Text style={styles.phone}>{phone}</Text>
+      </View>
+    </LinearGradient>
   );
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.safeArea}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Eco Express</Text>
+        </View>
         <View style={styles.container}>
           <Image source={user.profileImage} style={styles.profileImage} />
-          <ProfileCard name={user.name} email={user.email} />
+          <ProfileCard name={user.name} email={user.email} phone={savedPhoneNumber} />
           <View style={styles.row}>
             <TextInput
               style={styles.input}
               placeholder="Número de teléfono"
+              placeholderTextColor="gray"
               value={phoneNumber}
               onChangeText={setPhoneNumber}
               keyboardType="phone-pad"
             />
           </View>
-          <Button title="Guardar cambios" onPress={() => console.log('Cambios guardados:', phoneNumber)} />
+          <Button
+            title="Guardar cambios"
+            onPress={() => {
+              if (phoneNumber.trim() === '') {
+                alert('Por favor, ingresa un número de teléfono válido.');
+                return;
+              }
+              setSavedPhoneNumber(phoneNumber);
+              console.log('Cambios guardados:', phoneNumber);
+            }}
+          />
         </View>
       </SafeAreaView>
     </TouchableWithoutFeedback>
@@ -49,45 +75,43 @@ export default function Index() {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#f4f4f4', // Asegura que el fondo cubra toda el área segura
+    backgroundColor: '#f4f4f4',
   },
   container: {
     flex: 1,
     padding: 50,
-    alignItems: 'center', // Centra los elementos horizontalmente
-    justifyContent: 'center', // Centra el contenido verticalmente
-    backgroundColor: '#f4f4f4', // Un color de fondo claro
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f4f4f4',
   },
   profileImage: {
-    width: '100%', // Ajusta el ancho al 100% del contenedor padre
+    width: '100%',
     height: 300,
     borderTopLeftRadius: 20, 
     borderTopRightRadius: 20, 
-
-    marginBottom: 5, // Espacio debajo de la imagen
+    marginBottom: 5,
   },
   profileCard: {
-    width: '100%', // Define un ancho consistente para el card
+    width: '100%',
     alignItems: 'center',
     marginBottom: 20,
     padding: 15,
-    backgroundColor: 'white',
     borderBottomLeftRadius: 20, 
     borderBottomRightRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    elevation: 2, // Sombra para Android (para iOS se usan las propiedades shadow*)
   },
   name: {
     fontSize: 22,
     fontWeight: 'bold',
     marginBottom: 5,
+    color: 'white',
   },
   email: {
     fontSize: 16,
-    color: 'gray',
+    color: 'white',
+  },
+  phone: {
+    fontSize: 16,
+    color: 'white',
   },
   input: {
     height: 40,
@@ -105,5 +129,16 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10,
+  },
+  header: {
+    width: '100%',
+    padding: 20,
+    backgroundColor: '#045e96',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
